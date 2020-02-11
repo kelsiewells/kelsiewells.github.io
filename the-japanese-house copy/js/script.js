f1108abdbded9371296e05c8fc5e1c96c5704436
kelsiewells <59620231+kelsiewells@users.noreply.github.com>
2020-02-10 18:57:34 -0800

    large commit

---
+++ the-japanese-house copy/js/script.js
@@ -0,0 +1,18 @@
+// your script file for adding your own jquery
+$(function() {
+// Your Code from here on down. Don't delete that line above!
+  
+
+$("body").load(function(){
+  $("body").fadeOut(speed,callout);
+  $("#body").fadeOut("slow");
+  $("body").fadeOut(3000);
+});
+
+
+
+
+
+
+// End of Your Code . Don't delete that line below!!
+});
